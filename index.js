9663b57426c0c9f33bba1368111b223dcb8e456b
Harshbadgujars <hardyalsinghdaudsar@gmail.com>
2022-11-08 17:57:38 +0530

    Netflix

---
+++ index.js
@@ -0,0 +1,29 @@
+function myfunction1(){document.getElementById("a").style.display="block";};
+
+function myfunction2()
+{document.getElementById("a").style.display="none";}
+
+function myfunction3(){document.getElementById("b").style.display="block";};
+
+function myfunction4()
+{document.getElementById("b").style.display="none";}
+
+function myfunction5(){document.getElementById("c").style.display="block";};
+
+function myfunction6()
+{document.getElementById("c").style.display="none";}
+
+function myfunction7(){document.getElementById("d").style.display="block";};
+
+function myfunction8()
+{document.getElementById("d").style.display="none";}function myfunction(){document.getElementById("d").style.display="block";};
+
+function myfunction10()
+{document.getElementById("e").style.display="none";}
+
+function myfunction9(){document.getElementById("e").style.display="block";};
+
+function myfunction12()
+{document.getElementById("f").style.display="none";}
+
+function myfunction11(){document.getElementById("f").style.display="block";};
